5104426efacd90c9208ebb923fc997e6e2e4b3e9
Michael Lehn <michael.lehn@uni-ulm.de>
2026-04-08 16:24:07 +0200

    configure okular

--- install.sh
+++ install.sh
@@ -211,17 +211,17 @@ pip3 install --user --break-system-packages neovim-remote
 add_to_file "alias texvim='SOCKET=/tmp/nvim; rm -f \$SOCKET; nvim --listen \$SOCKET'" "$PROFILE"
 mkdir -p ~/.config
 if command -v kwriteconfig5 >/dev/null 2>&1; then
-    kwriteconfig5 --file okularpartrc --group "Core General" --key EditorType "Custom"
-    kwriteconfig5 --file okularpartrc --group "Core General" --key ExternalEditor "nvr --servername /tmp/nvim --remote +%l %f"
+    kwriteconfig5 --file okularpartrc --group "Core General" --key ExternalEditor "Custom"
+    kwriteconfig5 --file okularpartrc --group "Core General" --key ExternalEditorCommand "nvr --servername /tmp/nvim --remote +%l %f"
 elif command -v kwriteconfig6 >/dev/null 2>&1; then
-    kwriteconfig6 --file okularpartrc --group "Core General" --key EditorType "Custom"
-    kwriteconfig6 --file okularpartrc --group "Core General" --key ExternalEditor "nvr --servername /tmp/nvim --remote +%l %f"
+    kwriteconfig6 --file okularpartrc --group "Core General" --key ExternalEditor "Custom"
+    kwriteconfig6 --file okularpartrc --group "Core General" --key ExternalEditorCommand "nvr --servername /tmp/nvim --remote +%l %f"
 else
     cat >> ~/.config/okularpartrc <<'EOF'
 
 [Core General]
-EditorType=Custom
-ExternalEditor=nvr --servername /tmp/nvim --remote +%l %f
+ExternalEditor=Custom
+ExternalEditorCommand=nvr --servername /tmp/nvim --remote +%l %f
 EOF
 fi
 
